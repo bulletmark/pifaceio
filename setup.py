be45cee71b6c4eb70a2455de8a20900816a2baab
Mark Blakeney <mark.blakeney@bullet-systems.net>
2023-03-20 22:27:16 +1000

    Update to version 1.36
    Merely to get updated README installed to PyPI. Unfortunately the only
    way to do this is to update the release version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = Path(__file__).resolve().parent
 
 setup(
     name=name,
-    version='1.35',
+    version='1.36',
     description='Python interface to the Raspberry Pi PiFace board',
     long_description=here.joinpath('README.md').read_text(),
     long_description_content_type="text/markdown",
